b57e7655dac83201a12ffd26df0d25a08f2ebf0d
Shane Umayanga <shaneumayanga445@gmail.com>
2021-07-01 00:20:43 +0530

    chi

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"gosession/gosession"
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
@@ -17,9 +19,14 @@ func main() {
 		Expires:  time.Now().Add(time.Second * 60),
 	}, &gosession.SessionStoreOptions{})
 
-	http.ListenAndServe(":8080", session.NewGoSession(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			rw.Write([]byte("SHnae"))
-		}
-	})))
+	router := chi.NewRouter()
+	router.Use(session.NewGoSession)
+	router.Get("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("Hello there!"))
+	})
+	router.Get("/session", func(rw http.ResponseWriter, r *http.Request) {
+		cookie, _ := r.Cookie("session")
+		rw.Write([]byte(cookie.Value))
+	})
+	http.ListenAndServe(":8080", router)
 }
